e5f17156dbae1f03b4cbdc97a024a9e598854f89
paul <paul.christophe6@gmail.com>
2018-01-12 15:10:03 -0500

    sort the folder

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const { writeFile, lstatSync, readdirSync, readFileSync } = require('fs');
+const { writeFile, lstatSync, readdirSync, readFileSync, existsSync } = require('fs');
 const { join, dirname, resolve, parse } = require('path');
 const mkdirp = require('mkdirp');
+const YAML = require('yamljs');
 
+const docRoot = resolve(__dirname, './docs');
+
+// helper function to create folder if it doesn't exist, then write file
 const writeFileToFolder = (path, contents, cb) => {
   mkdirp(dirname(path), function (err) {
     if (err) return cb(err);
@@ -19,30 +23,48 @@ const getDirectories =
     readdirSync(source)
     .map(name => join(source, name))
 
-const contents = getDirectories(resolve(__dirname, './docs'));
+const contents = getDirectories(docRoot);
 
-const compileDirectory = (contents) =>
-  contents.reduce((acc, item) => {
-    const { name, ext } = parse(item);
+const compileDirectory = (contents, docRoot) => {
+  const orderPath = `${docRoot}/_order.yml`;
+  const order = existsSync(orderPath) ? YAML.load(orderPath) : null;
 
-    if (!isDirectory(item)) {
-      if (ext !== '.md') return acc;
+  return contents
+    .map(item => parse(item))
+    .sort((a, b) => {
+      const aOrder = order ? order.findIndex(name => name === a.name) : a.name;
+      const bOrder = order ? order.findIndex(name => name === b.name) : b.name;
 
-      const content = readFileSync(item, 'utf8');
+      if (aOrder < bOrder) return -1;
+      if (aOrder > bOrder) return 1;
+      return 0;
+    })
+    .reduce((acc, item) => {
+      const { base, name, ext, dir } = item;
+      const path = `${dir}/${base}`;
 
-      return [ ...acc, { name, content } ];
-    }
+      if (!isDirectory(path)) {
+        if (ext !== '.md') return acc;
 
-    return [
-      ...acc,
-      {
-        name,
-        content: compileDirectory(getDirectories(item)),
+        // if it's markdown, write contents to the object
+        const content = readFileSync(path, 'utf8');
+
+        return [ ...acc, { name, content } ];
       }
-    ];
-  }, []);
 
-writeFileToFolder('./build/test.json', JSON.stringify(compileDirectory(contents)), (err) => {
+      // if directory, recurse through the subdirector
+      return [
+        ...acc,
+        {
+          name,
+          content: compileDirectory(getDirectories(path), dir),
+        }
+      ];
+    }, []);
+}
+
+// write documentation json to the build folder
+writeFileToFolder('./build/test.json', JSON.stringify(compileDirectory(contents, docRoot)), (err) => {
   if (err) {
     return console.log(err);
   }
